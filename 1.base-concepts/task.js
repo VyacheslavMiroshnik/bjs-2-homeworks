c940364c8a00eea64241933d30da75c910402313
Vyacheslav Miroshnik <mepa087@gmail.com>
2023-02-08 01:02:09 +0300

    second homework done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,42 +1,49 @@
-"use strict"
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
+("use strict");
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  let discr = (Math.pow(b,2) - 4 * a * c) ;
-  if (discr >= 0 ){
-    if (discr === 0){
-      arr.push(-b/(2 * a));
+  let discr = Math.pow(b, 2) - 4 * a * c;
+  if (discr >= 0) {
+    if (discr === 0) {
+      arr.push(-b / (2 * a));
     } else if (discr > 0) {
-      arr.push((-b + Math.sqrt(discr) ) /  (2 * a) );
-      arr.push((-b - Math.sqrt(discr)  )  /(2 * a) );
-    };
+      arr.push((-b + Math.sqrt(discr)) / (2 * a));
+      arr.push((-b - Math.sqrt(discr)) / (2 * a));
+    }
+  }
 
-  };
-  
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let checkPercent = ((typeof(+ percent) === 'number' ) && !Number.isNaN(+ percent)) ? '' : `Параметр процент содержит неправильное значение ${percent} `;
-  let checkContribution = ((typeof(+ contribution) === 'number' ) && !Number.isNaN(+ contribution)) ? '' : `Параметр первоначальный взнос содержит неправильное значение ${contribution} `;
-  let checkAmount = ((typeof(+ amount) === 'number' ) && !Number.isNaN(+ amount)) ? '' : `Параметр сумма кредита содержит неправильное значение ${amount} `;
-  let checkCountMonth = ((typeof(+ countMonths) === 'number' ) && !Number.isNaN(+ countMonths)) ? '' : `Параметр дата окончания содержит неправильное значение ${countMonths} `;
-  if (checkPercent || checkContribution || checkAmount || checkCountMonth){
-   return !checkPercent && !checkContribution && !checkAmount && !checkCountMonth;
-  };
- let totalAmount;
- let clearAmount = amount - contribution;
- let percentByMonth = (percent / 100) / 12;
- let payByMonth = clearAmount * (percentByMonth + (percentByMonth/(Math.pow((1 + percentByMonth), countMonths) - 1)));
- totalAmount = +(payByMonth * countMonths).toFixed(2);
- return totalAmount;
+  let checkPercent =
+    typeof +percent === "number" && !Number.isNaN(+percent)
+      ? ""
+      : `Параметр процент содержит неправильное значение ${percent} `;
+  let checkContribution =
+    typeof +contribution === "number" && !Number.isNaN(+contribution)
+      ? ""
+      : `Параметр первоначальный взнос содержит неправильное значение ${contribution} `;
+  let checkAmount =
+    typeof +amount === "number" && !Number.isNaN(+amount)
+      ? ""
+      : `Параметр сумма кредита содержит неправильное значение ${amount} `;
+  let checkCountMonth =
+    typeof +countMonths === "number" && !Number.isNaN(+countMonths)
+      ? ""
+      : `Параметр дата окончания содержит неправильное значение ${countMonths} `;
+  if (checkPercent || checkContribution || checkAmount || checkCountMonth) {
+    return (
+      !checkPercent && !checkContribution && !checkAmount && !checkCountMonth
+    );
+  }
+  let totalAmount;
+  let clearAmount = amount - contribution;
+  let percentByMonth = percent / 100 / 12;
+  let payByMonth =
+    clearAmount *
+    (percentByMonth +
+      percentByMonth / (Math.pow(1 + percentByMonth, countMonths) - 1));
+  totalAmount = +(payByMonth * countMonths).toFixed(2);
+  return totalAmount;
 }
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,68 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    min = arr[i] < min ? arr[i] : min;
+    max = arr[i] > max ? arr[i] : max;
+    sum += arr[i];
+  }
+  let avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  let initValue = 0;
+  let resultSummElement = arr.reduce((sum, item) => sum + item, initValue);
+  return resultSummElement;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (!arr.length) {
+    return 0;
+  } else {
+    let min = Math.min.apply(null, arr);
+    let max = Math.max.apply(null, arr);
+    return max - min;
+  }
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  let eventSumm = 0;
+  let oddSumm = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2) {
+      oddSumm += arr[i];
+    } else {
+      eventSumm += arr[i];
+    }
+  }
+  return eventSumm - oddSumm;
 }
-
 function averageEvenElementsWorker(...arr) {
-
+  let evenCount = 0;
+  let sumCount = 0;
+  if (arr.length) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2) {
+        continue;
+      } else {
+        evenCount += 1;
+        sumCount += arr[i];
+      }
+    }
+  } else {
+    return 0;
+  }
+  return sumCount / evenCount;
 }
 
-function makeWork (arrOfArr, func) {
-
+function makeWork(arrOfArr, func) {
+  let maxWorkResult = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let funcResult = func(...arrOfArr[i]);
+    maxWorkResult = maxWorkResult < funcResult ? funcResult : maxWorkResult;
+  }
+  return maxWorkResult;
 }
