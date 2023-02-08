ae6f7ee0d40d4fcc25b26c60496dd25617a5e36a
Vyacheslav Miroshnik <mepa087@gmail.com>
2023-02-08 20:36:26 +0300

    homework 3 done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length) {
+    return arr1.every((element, i) => element === arr2[i]);
+  } else {
+    return false;
+  }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users
+    .filter((user) => user.gender === gender)
+    .map((user) => user.age)
+    .reduce(
+      (previous, current, i, arr) =>
+        i == arr.length - 1
+          ? (previous + current) / arr.length
+          : previous + current,
+      0
+    );
+  return result;
 }
